7da96bbca817a20d0625ff20941f50ca78bb48fe
Octavian25 <octaviansaja2@gmail.com>
2021-08-26 16:53:23 +0700

    Initial commit

---
+++ lib/ui/pages/splash_screen.dart
@@ -0,0 +1,11 @@
+part of 'page.dart';
+
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
